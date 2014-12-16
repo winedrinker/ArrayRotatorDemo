0963eddf6a4a00e833958d509f1627bf2b3a105d
Csaba Tölcsér <cstolcser@gmail.com>
2014-12-16 19:23:50 +0100

    Implemented demo and test

---
+++ org.array.rotate/src/org/array/rotate/ArrayRotatorDemo.java
@@ -0,0 +1,115 @@
+package org.array.rotate;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * Small class to show ArrayRotator class usage.
+ *
+ * @author csaba
+ *
+ */
+public class ArrayRotatorDemo {
+
+	public static void main(final String[] args) {
+
+		new ArrayRotator().run();
+	}
+
+	/**
+	 * Class to manage user inputs from the console which are the positions and an array of integers.
+	 * Rotate the array with the given positions and print original and rotated array to the console.
+	 *
+	 * @author csaba
+	 *
+	 */
+	final protected static class ArrayRotator {
+
+		private static final int NO_MORE_INPUT = -1344099;
+
+		private static final String POS_MESSAGE = "Please enter positions for the array rotation: ";
+		private static final String ANY_KEY_MESSAGE = "(Type any key + enter to quit)";
+		private static final String ARRAY_MESSAGE = "Please enter the integers for the array: ";
+
+		private static final String ORIG_ARRAY_MESSAGE = "The original array was: ";
+		private static final String ROTATED_ARRAY_MESSAGE = "The rotated array is: ";
+
+
+		private final Scanner scanner;
+		private int positions;
+		private final List<Integer> inputArray = new ArrayList<>();
+		private final List<Integer> rotatedArray = new ArrayList<>();
+
+		protected ArrayRotator() {
+			super();
+			this.scanner = new Scanner(System.in);
+		}
+
+		protected void run() {
+
+			// Read positions from the console
+			System.out.println(POS_MESSAGE);
+			System.out.println(ANY_KEY_MESSAGE);
+			positions = getNextInt();
+
+			// If the input positions is not an integer exist from the program
+			if (positions == NO_MORE_INPUT)
+				System.exit(0);
+
+			// Read input array from the console
+			System.out.println(ARRAY_MESSAGE);
+			System.out.println(ANY_KEY_MESSAGE);
+			while (scanner.hasNextInt()) {
+
+				inputArray.add(scanner.nextInt());
+			}
+			
+			// If the input array is not set exist from the program
+			if (inputArray.isEmpty())
+				System.exit(0);
+
+			rotate();
+
+			// Print results to the console
+			System.out.println(ORIG_ARRAY_MESSAGE);
+			System.out.println(inputArray);
+
+			System.out.println(ROTATED_ARRAY_MESSAGE);
+			System.out.println(rotatedArray);
+		}
+
+		/**
+		 * Read the next input from the scanner.
+		 *
+		 * @return the integer value of the input or NO_MORE_INPUT constant if the input was not an integer
+		 */
+		private int getNextInt() {
+
+			return scanner.hasNextInt() ? scanner.nextInt() : NO_MORE_INPUT;
+		}
+
+		/**
+		 * Rotate the input {@link ArrayList} with the number of positions and add new values
+		 * to rotatedArray;
+		 *
+		 * @param positions the number which will be use to rotate array
+		 * @param inputArrayList the array which will be rotate
+		 */
+		public void rotate() {
+
+			rotatedArray.addAll(inputArray);
+			Collections.rotate(rotatedArray, positions);
+		}
+
+		protected List<Integer> getInputArray() {
+			return inputArray;
+		}
+
+		protected List<Integer> getRotatedArray() {
+			return rotatedArray;
+		}
+
+	}
+}
---
+++ org.array.rotate/test/org/array/rotate/ArrayRotatorTest.java
@@ -0,0 +1,121 @@
+package org.array.rotate;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.array.rotate.ArrayRotatorDemo.ArrayRotator;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.contrib.java.lang.system.ExpectedSystemExit;
+import org.junit.contrib.java.lang.system.TextFromStandardInputStream;
+
+
+public class ArrayRotatorTest {
+
+	@Rule
+	public final TextFromStandardInputStream mockInput = TextFromStandardInputStream.emptyStandardInputStream();
+
+	@Rule
+	public final ExpectedSystemExit exit = ExpectedSystemExit.none();
+
+	/**
+	 * Test case when the first input is not an integer and the program quit immediately.
+	 *
+	 * @throws InterruptedException
+	 */
+	@Test
+	public void testRotator_QuitImmediately() throws InterruptedException {
+
+		mockInput.provideText("q"); //$NON-NLS-1$
+		exit.expectSystemExitWithStatus(0);
+		new ArrayRotator().run();
+	}
+
+	/**
+	 * Test case when the positions is set, but the input array is not defined by the user, so that is empty.
+	 *
+	 * @throws InterruptedException
+	 */
+	@Test
+	public void testRotator_NoArray() throws InterruptedException {
+
+		mockInput.provideText("1"); //$NON-NLS-1$
+		exit.expectSystemExitWithStatus(0);
+		final ArrayRotator rotator = new ArrayRotator();
+		rotator.run();
+
+		assertTrue("Input Array should be empty, because that is not typed by the user!", rotator.getInputArray().isEmpty());
+		assertTrue("Rotated Array should be empty, because input array was empty!", rotator.getRotatedArray().isEmpty());
+	}
+
+	/**
+	 * Test case when the positions is set, but the next input is a key which involve the quit.
+	 *
+	 * @throws InterruptedException
+	 */
+	@Test
+	public void testRotator_SetPositionsAndQuit() throws InterruptedException {
+
+		mockInput.provideText("1 q"); //$NON-NLS-1$
+		exit.expectSystemExitWithStatus(0);
+		final ArrayRotator rotator = new ArrayRotator();
+		rotator.run();
+
+		assertTrue("Input Array should be empty, because quit from the program!", rotator.getInputArray().isEmpty());
+		assertTrue("Rotated Array should be empty, because input array was empty!", rotator.getRotatedArray().isEmpty());
+	}
+
+	/**
+	 * Test case when the positions is a positive integer and the input array also is set.
+	 * The positions is 2.
+	 * The input array is: [5, 10, 12, 128, 3]
+	 * The expected rotated array is: [128, 3, 5, 10, 12]
+	 *
+	 * @throws InterruptedException
+	 */
+	@Test
+	public void testRotator_PositionPositive() throws InterruptedException {
+
+		mockInput.provideText("2 5 10 12 128 3"); //$NON-NLS-1$
+		final ArrayRotator rotator = new ArrayRotator();
+		rotator.run();
+
+		final ArrayList<Integer> expectedOriginal = getExpectedOriginalArray();
+		final ArrayList<Integer> expectedRotated = new ArrayList<>(Arrays.asList(128, 3, 5, 10, 12));
+
+		assertEquals(expectedOriginal, rotator.getInputArray());
+		assertEquals(expectedRotated, rotator.getRotatedArray());
+	}
+
+	/**
+	 * Test case when the positions is a negative integer and the input array also is set.
+	 * The positions is -2.
+	 * The input array is: [5, 10, 12, 128, 3]
+	 * The expected rotated array is: [12, 128, 3, 5, 10]
+	 *
+	 * @throws InterruptedException
+	 */
+	@Test
+	public void testRotator_PositionNegative() throws InterruptedException {
+
+		mockInput.provideText("-2 5 10 12 128 3"); //$NON-NLS-1$
+		final ArrayRotator rotator = new ArrayRotator();
+		rotator.run();
+
+		final ArrayList<Integer> expectedOriginal = getExpectedOriginalArray();
+		final ArrayList<Integer> expectedRotated = new ArrayList<>(Arrays.asList(12, 128, 3, 5, 10));
+
+		assertEquals(expectedOriginal, rotator.getInputArray());
+		assertEquals(expectedRotated, rotator.getRotatedArray());
+	}
+
+	private static ArrayList<Integer> getExpectedOriginalArray() {
+		return new ArrayList<>(Arrays.asList(5, 10, 12, 128, 3));
+	}
+
+
+
+}
